7f8040d7f02e5cb272a67d35cd62e7734fccd747
EthanHua <hope.86@live.cn>
2016-04-07 20:50:36 +0800

    add root view

--- Pitaya/www/www/urls.py
+++ Pitaya/www/www/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+# www root view
+from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^myhealth', include('myhealth.urls')),
+    url(r'^$', views.index, name='index'),
 ]
---
+++ Pitaya/www/www/views.py
@@ -0,0 +1,6 @@
+# www root view
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, this is Pitaya!")
+
