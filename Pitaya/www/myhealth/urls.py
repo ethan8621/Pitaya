1746f7a0be72e575dfc24fee5ef7bddaa74585f7
t-huac <chao.hua@autodesk.com>
2016-04-08 17:20:00 +0800

    django part4a

--- Pitaya/www/myhealth/urls.py
+++ Pitaya/www/myhealth/urls.py
@@ -2,6 +2,14 @@
 
 from . import views
 
+app_name = 'myhealth'
 urlpatterns = [
+    # ex: /myhealth/
     url(r'^$', views.index, name='index'),
+    # ex: /myhealth/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /myhealth/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /myhealth/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
