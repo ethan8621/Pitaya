5d8835b4b062b80d8d92a816b1483f5aa9b53965
t-huac <chao.hua@autodesk.com>
2016-04-14 15:04:39 +0800

    add userregister project

---
+++ Pitaya/www/userregister/models.py
@@ -0,0 +1,9 @@
+﻿from django.db import models
+
+# database User table
+class User(models.Model):
+    user_id = models.CharField(max_length=200)
+    user_email = models.CharField(max_length=200)
+    admin_privilege = models.BooleanField()
+    user_password = models.CharField(max_length=200)
+
